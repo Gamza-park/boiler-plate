d04b131f06d81864788b5a844f34ad300c6a4ec9
박형일 <gamza@baghyeong-il-ui-MacBookPro.local>
2020-07-11 21:56:44 +0900

    Add SignIn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
+// application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://gamza:guddlf28@cluster0.ot65i.mongodb.net/test?retryWrites=true&w=majority', {
@@ -12,5 +18,19 @@ mongoose.connect('mongodb+srv://gamza:guddlf28@cluster0.ot65i.mongodb.net/test?r
 
 
 app.get('/', (req, res) => res.send('hello world!'));
+
+app.post('/register', (req, res) => {
+    // Get SignIn info for client 
+    // inser DataBase
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err});
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
